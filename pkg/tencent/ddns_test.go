066fea60f5a52c652b99dbf0a583cbeaaf0953ba
notes-bin <17797274906@163.com>
2025-01-24 10:03:24 +0800

    docs: 删除冗余注释

--- pkg/tencent/ddns_test.go
+++ pkg/tencent/ddns_test.go
@@ -3,6 +3,8 @@ package tencent
 import (
 	"os"
 	"testing"
+
+	"github.com/notes-bin/ddns6/utils"
 )
 
 // 需要设置环境变量 TENCENTCLOUD_SECRET_ID
@@ -16,13 +18,19 @@ var domain = os.Getenv("DOMAIN")
 
 var tc = New(secretId, secretKey)
 
-func TestRequest(t *testing.T) {
-
+func TestListRecords(t *testing.T) {
+	response := new(TencentCloudResponse)
+	err := tc.ListRecords(domain, response)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("response -> %#v\n", response)
 }
 
-func TestListRecords(t *testing.T) {
-	var response = new(TencentCloudResponse)
-	err := tc.ListRecords(domain, response)
+func TestCreateRecord(t *testing.T) {
+	response := new(TencentCloudStatus)
+	ipv6, _ := utils.NewPublicDNS().GetIPV6Addr()
+	err := tc.CreateRecord(domain, "www", ipv6[0].String(), response)
 	if err != nil {
 		t.Error(err)
 	}
