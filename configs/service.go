662dce8f25cbce29ceb9d58e7d6e2d48b5cccb0a
notes-bin <17797274906@163.com>
2025-02-04 21:33:19 +0800

    chore: 优化

--- configs/service.go
+++ configs/service.go
@@ -2,6 +2,7 @@ package configs
 
 import (
 	_ "embed"
+	"log/slog"
 	"os"
 	"strings"
 	"text/template"
@@ -20,34 +21,36 @@ func GenerateService(params ...string) {
 	// 获取当前目录
 	dir, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		slog.Error("获取当前目录失败", "err", err)
+		os.Exit(1)
 	}
-
-	paramsStr := strings.Join(params, " ")
 
 	// 解析模板
 	tmpl, err := template.New("service").Parse(content)
 	if err != nil {
-		panic(err)
+		slog.Error("解析模板失败", "err", err)
+		os.Exit(1)
 	}
 
 	// 定义服务配置
 	config := ServiceConfig{
 		Description:      "ddns6 auto update service",
-		ExecStart:        paramsStr,
+		ExecStart:        strings.Join(params, " "),
 		WorkingDirectory: dir,
 	}
 
 	// 创建输出文件
 	outputFile, err := os.Create("ddns6-update.service")
 	if err != nil {
-		panic(err)
+		slog.Error("创建输出文件失败", "err", err)
+		os.Exit(1)
 	}
 	defer outputFile.Close()
 
 	// 执行模板并写入文件
 	err = tmpl.Execute(outputFile, config)
 	if err != nil {
-		panic(err)
+		slog.Error("执行模板失败", "err", err)
+		os.Exit(1)
 	}
 }
