3419c1bc14d8e7384ab42bdcd76794af104c8e8d
notes-bin <17797274906@163.com>
2026-02-03 10:53:49 +0800

    fix: 修复因获取ipv6地址功能变化引起的错误

--- internal/domain/recorde.go
+++ internal/domain/recorde.go
@@ -12,11 +12,6 @@ import (
 // 定义自定义错误类型
 var ErrEmptyIPv6Address = errors.New("获取到的 IPv6 地址为空")
 
-// IPv6Getter 定义获取 IPv6 地址的接口，使用 context 支持取消操作
-type IPv6Getter interface {
-	GetIPv6Addr(ctx context.Context) (net.IP, error)
-}
-
 // Tasker 定义执行 DNS 更新任务的接口
 type Tasker interface {
 	Task(domain, subdomain, ipv6addr string) error
@@ -41,8 +36,10 @@ func (d *Domain) String() string {
 	return fmt.Sprintf("fullDomain: %s, type: %s, addr: %s", fullDomain, d.Type, d.Addr)
 }
 
+type ipv6GetterFunc func(ctx context.Context) (net.IP, error)
+
 // UpdateRecord 更新 DNS 记录
-func (d *Domain) UpdateRecord(ctx context.Context, ipv6Getter IPv6Getter, tasker Tasker) error {
+func (d *Domain) UpdateRecord(ctx context.Context, ipv6 net.IP, tasker Tasker) error {
 	select {
 	case <-ctx.Done():
 		slog.Info("更新任务被取消", "domain", d.Domain, "subdomain", d.SubDomain)
@@ -51,46 +48,12 @@ func (d *Domain) UpdateRecord(ctx context.Context, ipv6Getter IPv6Getter, tasker
 		d.mu.Lock()
 		defer d.mu.Unlock()
 
-		addr, err := d.getIPv6Address(ctx, ipv6Getter)
-		if err != nil {
-			return d.handleError("获取 IPv6 地址失败", err)
-		}
-
-		if d.hasAddressChanged(addr) {
-			return d.updateDNSRecord(tasker, addr)
+		if d.hasAddressChanged(ipv6) {
+			return d.updateDNSRecord(tasker, ipv6)
 		}
 		slog.Info("IPv6 地址未改变，无需更新", "domain", d.Domain, "subdomain", d.SubDomain)
 		return nil
 	}
-}
-
-// getIPv6Address 获取 IPv6 地址，增加重试机制和日志记录
-func (d *Domain) getIPv6Address(ctx context.Context, ipv6Getter IPv6Getter) (net.IP, error) {
-	const maxRetries = 3 // 最大重试次数
-	var addr net.IP
-	var err error
-
-	for i := range maxRetries {
-		if ctx.Err() != nil {
-			return nil, ctx.Err() // 如果上下文已取消，直接返回错误
-		}
-
-		slog.Info("尝试获取 IPv6 地址", "domain", d.Domain, "subdomain", d.SubDomain, "attempt", i+1)
-		addr, err = ipv6Getter.GetIPv6Addr(ctx)
-
-		if err == nil {
-			if addr == nil {
-				err = ErrEmptyIPv6Address
-			} else {
-				slog.Info("成功获取 IPv6 地址", "domain", d.Domain, "subdomain", d.SubDomain, "ipv6", addr.String())
-				return addr, nil
-			}
-		}
-
-		slog.Error("获取 IPv6 地址失败", "domain", d.Domain, "subdomain", d.SubDomain, "err", err, "attempt", i+1)
-	}
-
-	return nil, fmt.Errorf("多次尝试后仍无法获取 IPv6 地址: %w", err)
 }
 
 // updateDNSRecord 更新 DNS 记录
